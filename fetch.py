e89590bae5321430742b279c549f8ff934ffdd4c
SingAvi <githubitis@14>
2019-02-08 02:26:08 +0530

    Full Working

--- fetch.py
+++ fetch.py
@@ -1,20 +1,55 @@
 import urllib2
 import json
 
-InputMovie = input("Enter Movie Name: ")
+# http://www.omdbapi.com/?t=MovieName&apikey=31f9c50
+
 
 def movieSearch(query):
     key = '31f9c50'
 
-    url = urllib2.urlopen('http://www.omdbapi.com/?t=' + query + '&apikey=' + key)
+    url = urllib2.urlopen('http://www.omdbapi.com/?t=' + str(query) + '&apikey=' + str(key))
     data = json.load(url)
 
     print(data['Title'])
     print(data['Year'])
+    print(data['Released'])
+    print(data['Director'])
+    print(data['Website'])
+    print(data['Production'])
+    print(data['imdbRating'])
+
+def reSearch():
+
+    print('\n')
+    print("Do you wanna search another movie?")
+
+    x = raw_input("Y/N ? =>")
+    if(x == 'Y' or x == 'y'):
+        y = raw_input("Enter Movie:")
+        movieSearch(y)
+
+
+    else:
+        quit()
 
 
 
-movieSearch(InputMovie)
+if __name__ == "__main__":
+
+    InputMovie = raw_input("Enter Movie Name:")
+
+    movieSearch(InputMovie)
+    reSearch()
+
+
+
+
+
+
+
+
+
+
 
 
 
